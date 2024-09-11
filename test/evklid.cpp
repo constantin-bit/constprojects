e612d15eb65cb8ddc983e4c25665bfc797cfab6d
constantin-bit <const.larin@gmail.com>
2024-09-11 23:05:24 +0300

    Update evklid.cpp

--- test/evklid.cpp
+++ test/evklid.cpp
@@ -7,10 +7,10 @@ using namespace std;
 int main(){
     int a,b;
     cin >> a >> b;
-    while ((a!=0)&&(b!=0)){
-        if (a>b) a=a%b;
-        else b=b%a;
+    while ((a != 0) && (b != 0)){
+        if (a > b) a = a % b;
+        else b = b % a;
     }
-    cout << a+b;
+    cout << a + b;
     return 0;
 }
