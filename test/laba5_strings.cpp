a734056793ac6f31cd718effb6de42f34cb89a84
constantin-bit <const.larin@gmail.com>
2024-10-13 19:50:20 +0300

    Create laba5_strings.cpp

---
+++ test/laba5_strings.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <fstream>
+
+using namespace std;
+
+int main(){
+    ifstream fin("..\\files_for_labs\\for_laba5.txt");
+    string line;
+    int line_number = 0, j = 0, count = 0;
+    while (getline(fin, line)){
+        int i = 0;
+        while (i < line.length()){
+            if ((line[i] == ' ') || (line[i] == '\t')) line.replace (i, 1, "");
+            else i += 1;
+        }
+        if (line.length() == 0){
+            count += 1;
+        }
+        else{
+            if (count != 0){
+                cout << count << ' ' << j + 1 << endl;
+            } 
+            j = line_number;
+            count = 0;
+        }
+        line_number += 1;
+    }
+    fin.close();
+    return 0;
+}
